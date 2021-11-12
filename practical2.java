5454efba81a496a4a44aee55d14ef71d8dd94bcb
Prabhjot Kaur <jotp908@gmail.com>
2021-11-11 17:36:23 -0930

    practical 1 to 3

---
+++ practical2.java
@@ -0,0 +1,11 @@
+
+public class practical2 {
+    public static void main(String[] args) {
+        int a = 15;
+        float b =  a;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+    
+}
---
+++ practical3.java
@@ -0,0 +1,32 @@
+
+
+public class practical3 {
+    public static void main(String[] args) {
+        // 1-d array
+        int[] arr = {12, 25, 36, 85, 96, 78};
+        for(int i =0;i<arr.length;i++){
+
+            System.out.print(arr[i] + "  ");
+           
+
+        }
+        System.out.println("");
+
+       
+
+        //2-d array
+
+        char[][] arr1 = {{'a','o'},{'b','c'},{'d','e'}};
+        for(int i =0;i<arr1.length;i++){
+            for(int j = 0 ; j< arr1[1].length;j++){
+                System.out.print(arr1[i][j] + "  ");
+   }
+
+   System.out.println("");
+
+        }
+        System.out.println("");
+
+  }
+    
+}
